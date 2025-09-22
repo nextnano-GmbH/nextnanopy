3c03364f4748a407f7f0089877be71cefc589a3c
Heorhii nn-mu <heorhii.yehiazarian@nextnano.com>
2025-09-22 16:40:09 +0200

    add utils.plotting with colors

---
+++ nextnanopy/utils/plotting.py
@@ -0,0 +1,31 @@
+"""Defines colors and styles for nextnano plots."""
+from matplotlib.colors import LinearSegmentedColormap
+
+NXT_BLUE = (19/256, 173/256, 181/256)
+WILD_STRAWBERRY = (255/256, 41/256, 103/256)
+GOLDEN = (253/256, 188/256, 66/256)
+GREEN = (0/256, 155/256, 85/256)
+
+_NXT_COLORS = [
+    (1, 1, 1),  # white
+    NXT_BLUE,
+    (0, 0, 0),  # black
+    WILD_STRAWBERRY,
+    (1, 1, 1),  # white
+]
+
+_NXT_BLUE_COLORS = [
+    (0, 0, 0),  # black
+    NXT_BLUE,
+    (1, 1, 1),  # white
+]
+
+_NXT_STRAWBERRY_COLORS = [
+    (0, 0, 0),  # black
+    WILD_STRAWBERRY,
+    (1, 1, 1),  # white
+]
+
+NXT_COLORMAP = LinearSegmentedColormap.from_list("NXT", _NXT_COLORS, N=1042)
+NXT_BLUE_COLORMAP = LinearSegmentedColormap.from_list("NXT_BLUE", _NXT_BLUE_COLORS, N=1042)
+NXT_STRAWBERRY_COLORMAP = LinearSegmentedColormap.from_list("NXT_STRAWBERRY", _NXT_STRAWBERRY_COLORS, N=1042)
