9ba027d64d475f7acd6a97abcbe2d97dc295a47e
Heorhii nn-mu <heorhii.yehiazarian@nextnano.com>
2025-09-22 17:15:19 +0200

    add mlp style

--- nextnanopy/utils/plotting.py
+++ nextnanopy/utils/plotting.py
@@ -1,10 +1,10 @@
 """Defines colors and styles for nextnano plots."""
 from matplotlib.colors import LinearSegmentedColormap
 
-NXT_BLUE = (19/256, 173/256, 181/256)
-WILD_STRAWBERRY = (255/256, 41/256, 103/256)
-GOLDEN = (253/256, 188/256, 66/256)
-GREEN = (0/256, 155/256, 85/256)
+NXT_BLUE = (19/256, 173/256, 181/256) #13adb5 
+WILD_STRAWBERRY = (255/256, 41/256, 103/256) #ff2966
+GOLDEN = (253/256, 188/256, 66/256) #fdbc42
+GREEN = (0/256, 155/256, 85/256) #009b55
 
 _NXT_COLORS = [
     (1, 1, 1),  # white
@@ -29,3 +29,10 @@ _NXT_STRAWBERRY_COLORS = [
 NXT_COLORMAP = LinearSegmentedColormap.from_list("NXT", _NXT_COLORS, N=1042)
 NXT_BLUE_COLORMAP = LinearSegmentedColormap.from_list("NXT_BLUE", _NXT_BLUE_COLORS, N=1042)
 NXT_STRAWBERRY_COLORMAP = LinearSegmentedColormap.from_list("NXT_STRAWBERRY", _NXT_STRAWBERRY_COLORS, N=1042)
+
+import matplotlib.pyplot as plt
+import importlib.resources as pkg_resources
+
+def use_nxt_style():
+    with pkg_resources.path("nextnanopy.utils.styles", "nxt_style.mplstyle") as style_path:
+        plt.style.use(style_path)
