b2f82ecd17882127987a3be0f9e85802872f34dd
Heorhii Yehiazarian <gevorgvandervaals@gmail.com>
2025-11-13 21:39:55 +0100

    updated cbr transmission example

--- templates/EPJQT2022_1D_slice_TCW_nnp.py
+++ templates/EPJQT2022_1D_slice_TCW_nnp.py
@@ -31,116 +31,88 @@ E = E_meV * 1.6e-19
 software = 'nextnano++' # Specify software product here!
 software_short = '_nnp'
 
-folder_input = nn.config.config[software]['inputdirectory'] # path to the input file set in nnpy config file, change if needed
-folder_output = nn.config.config[software]['outputdirectory'] # Define output folders. If they do not exist, they are created.
-folder_output_python = os.path.join(folder_output, r'nextnanopy')
-mkdir_if_not_exist(folder_output)
-mkdir_if_not_exist(folder_output_python)
+folder_input = r"C:\Program Files\nextnano\2025_09_18\nextnano++\examples\transmission"
 
-my_input_file_no_extension_nnp = r'EPJQT2022_2D_TCW_nnp'
-
-FileExtension = '.in'  # for nextnano++ and nextnano3
-my_input_file_no_extension = my_input_file_no_extension_nnp
-my_input_file = my_input_file_no_extension+FileExtension
+my_input_file = 'EPJQT2022_2D_TCW_nnp.nnp'
 
 print("starting nextnano...")
-input_file_name = os.path.join(folder_input, my_input_file)
-input_file = nn.InputFile(input_file_name)
+input_file_path = os.path.join(folder_input, my_input_file)
+input_file = nn.InputFile(input_file_path)
+input_file.save(temp=True)
+temp_path = input_file.fullpath
 
-print(f"List of variables: {input_file.variables}")
-for var in input_file.variables.values():
-    print(f'{var.text}')
+sweep_variable = 'tunnel_barrier_height'
 
-SweepVariable = 'tunnel_barrier_height'
-comment_original = input_file.variables[SweepVariable].comment
+sweep_values = [i*i for i in np.linspace(0.00, 1.00, 41)]
 
-SweepValues = [i*i for i in np.linspace(0.00, 1.00, 41)]
+sweep = nn.Sweep({sweep_variable: sweep_values}, fullpath=temp_path)
+print("Performing sweep for 2D simulation...")
+sweep.save_sweep(integer_only_in_name=True)
+sweep.execute_sweep(show_log=False, delete_input_files=True, parallel_limit=3)
 
-dfV=[]
-df_cbV=[]
-SweepVariableStringV=[]
 transmission_13_at_fixed_energy=[]
 transmission_14_at_fixed_energy=[]
 
-for x in [str(x) for x in SweepValues]:
-    
+sweep_infodict = sweep.sweep_output_infodict
 
-    input_file.set_variable(SweepVariable, value=x, comment='<= PYTHON <=' + comment_original)
-    
-    for var in input_file.variables.values():
-        print(var.text)      # --> better method to preview
-    
-    SweepVariableString = SweepVariable + '=' + x
-    SweepVariableStringV.append(SweepVariableString)
-    
-    input_file_name_variable = my_input_file_no_extension + '; ' + SweepVariableString
-    my_input_file_new = os.path.join(folder_output, input_file_name_variable + FileExtension)
-    print(my_input_file_new)
-    
-    input_file.save(my_input_file_new, overwrite=True, automkdir=True)
-    
-    input_file.execute() # Execute nextnano software. Put line into comment if you only want to to post-processing of results
-    
-    file    = os.path.join(folder_output,input_file_name_variable + r'\bias_00000' + r'\transmission_device_Gamma1.dat') 
-    
-    df    = nn.DataFile(file, product=software)
-    dfV.append(df)
-    
-    transmission_13_at_fixed_energy_x = df.variables['1->3'].value[int((E_meV - input_file.variables['E_min'].value)/input_file.variables['delta_energy'].value) + 1]
+barrier_values = []
+for folder_path, var_dict in sweep_infodict.items():
+    var_value = var_dict[sweep_variable]
+    dfolder = nn.DataFolder(folder_path)
+    file = dfolder.go_to('bias_00000', r"CBR", r"device", r"Gamma",  r"transmission.dat") 
+    df = nn.DataFile(file, product=software)
+    transmission_13_at_fixed_energy_x = df.variables['lead_1->lead_3'].value[int((E_meV - input_file.variables['E_min'].value)/input_file.variables['energy_resolution'].value) + 1]
     transmission_13_at_fixed_energy.append(transmission_13_at_fixed_energy_x)
-    transmission_14_at_fixed_energy_x = df.variables['1->4'].value[int((E_meV - input_file.variables['E_min'].value)/input_file.variables['delta_energy'].value) + 1]
+    transmission_14_at_fixed_energy_x = df.variables['lead_1->lead_4'].value[int((E_meV - input_file.variables['E_min'].value)/input_file.variables['energy_resolution'].value) + 1]
     transmission_14_at_fixed_energy.append(transmission_14_at_fixed_energy_x)
-    print("Read in file:")
-    print("current datafile: ",file)
-    print(f"List of coordinates in the current datafile: {df.coords}")
-    print(f"List of variables in the current datafile: {df.variables}")
+    barrier_values.append(var_value)
 
-my_input_file_no_extension_nnp = r'EPJQT2022_1D_slice_TCW_nnp'
+# sort plot_values and corresponding transmission values
+sorted_indices = np.argsort(barrier_values)
+sweep_values = [barrier_values[i] for i in sorted_indices]
+transmission_13_at_fixed_energy = [transmission_13_at_fixed_energy[i] for i in sorted_indices]
+transmission_14_at_fixed_energy = [transmission_14_at_fixed_energy[i] for i in sorted_indices]
 
-my_input_file_no_extension = my_input_file_no_extension_nnp
-my_input_file = my_input_file_no_extension+FileExtension
 
-print("starting nextnano...")
+
+# 1D slice simulation
+
+my_input_file = r'EPJQT2022_1D_slice_TCW_nnp.nnp'
+
+print("Performing sweep for 1D slice simulation...")
 input_file_name = os.path.join(folder_input, my_input_file)
 input_file = nn.InputFile(input_file_name)
+input_file.save(temp=True)
+temp_path = input_file.fullpath
 
-print(f"List of variables: {input_file.variables}")
-for var in input_file.variables.values():
-    print(f'{var.text}') # --> better method to preview
     
-SweepVariable = 'tunnel_barrier_height'
-comment_original = input_file.variables[SweepVariable].comment
+sweep_variable = 'tunnel_barrier_height'
 
-SweepValues2 = [i**3 for i in np.linspace(0.18, 1.00, 41)]
+sweep_values_2 = [i**3 for i in np.linspace(0.18, 1.00, 41)]
 
-dfV=[]
-df_cbV=[]
-SweepVariableStringV=[]
+sweep = nn.Sweep(variables_to_sweep={sweep_variable: sweep_values_2}, fullpath=temp_path)
+
+sweep.save_sweep(integer_only_in_name=True)
+sweep.execute_sweep(show_log=False, delete_input_files=True, parallel_limit=3)
+
+sweep_output_directory = sweep.sweep_output_directory
+
+sweep_dfolder = nn.DataFolder(sweep_output_directory)
 
 E_s = []
 E_a = []
 value_list = []
 delta_k_list = []
 scaling_factor = 44
+sweep_infodict = sweep.sweep_output_infodict
 
-for x in [str(x) for x in SweepValues2]:
-    
-    input_file.set_variable(SweepVariable, value=x, comment='<= PYTHON <=' + comment_original)
-    
-    SweepVariableString = SweepVariable + '=' + x
-    SweepVariableStringV.append(SweepVariableString)
-    
-    input_file_name_variable = my_input_file_no_extension + '; ' + SweepVariableString    
-    my_input_file_new = os.path.join(folder_output, input_file_name_variable + FileExtension)
-    print(my_input_file_new)
-    
-    input_file.save(my_input_file_new, overwrite=True, automkdir=True)
-    input_file.execute() # Execute nextnano software. Put line into comment if you only want to to post-processing of results
-    file    = os.path.join(folder_output,input_file_name_variable + r'\bias_00000\Quantum' + r'\energy_spectrum_cbr_Gamma_00000.dat') 
-    
+barrier_values = []
+for folder_path, var_dict in sweep_infodict.items():
+    var_value = var_dict[sweep_variable]
+    dfolder = nn.DataFolder(folder_path)
+    file  = dfolder.go_to("bias_00000" , "Quantum", "cbr", "Gamma", "energy_spectrum_k00000.dat") 
     df    = nn.DataFile(file, product=software)
-    dfV.append(df)
-    
+
     E_s_x = df.variables['Energy'].value[0] 
     E_a_x = df.variables['Energy'].value[1] 
     
@@ -151,14 +123,23 @@ for x in [str(x) for x in SweepValues2]:
     delta_k_list.append(delta_k)
     value = math.cos(delta_k*device_length/scaling_factor)**2  # add **2 to square
     value_list.append(value)
+    barrier_values.append(var_value)
     
+# sort the values
+sorted_indices = np.argsort(barrier_values)
+sweep_values_2 = [barrier_values[i] for i in sorted_indices]
+value_list = [value_list[i] for i in sorted_indices]
+
+
+
+
 fig, ax = plt.subplots(1)
 
-ax.plot(SweepValues, transmission_13_at_fixed_energy, 'c-', label='1->3')
-ax.plot(SweepValues, transmission_14_at_fixed_energy, 'g-', label='1->4')
+ax.plot(sweep_values, transmission_13_at_fixed_energy, 'c-', label='1->3')
+ax.plot(sweep_values, transmission_14_at_fixed_energy, 'g-', label='1->4')
 
-ax.plot(SweepValues2, value_list, 'c.') 
-ax.plot(SweepValues2, [1-x for x in value_list], 'g.')  
+ax.plot(sweep_values_2, value_list, 'c.') 
+ax.plot(sweep_values_2, [1-x for x in value_list], 'g.')  
 
 ax.set_xlim(-0.00,1.0)
 ax.set_ylim(0.0,1.0)
@@ -167,5 +148,5 @@ ax.set_ylabel('Transmission', fontsize=14)
 ax.set_xticks(np.arange(0, 1.1, 0.1))
 
 #fig.savefig('dotsandlines' + '.png', dpi=500, bbox_inches='tight',pad_inches = 0, transparent=True )
-
+plt.show()
 print('Done nextnanopy.')  
